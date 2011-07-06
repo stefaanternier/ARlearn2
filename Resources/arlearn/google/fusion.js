198f4c4c595c17c975ced71544d93a83e6c5a535
Stefaan Ternier <stefaan.ternier@ou.nl>
2011-07-06 12:24:55 +0200

    adding license information

--- Resources/arlearn/google/fusion.js
+++ Resources/arlearn/google/fusion.js
@@ -1,4 +1,12 @@
-(function() {
+/**
+ * ARLearn Platform
+ * Copyright (c) 2009-2011 by Open Universiteit. All Rights Reserved.
+ * ARLearn2 is licensed under the terms of GNU GENERAL PUBLIC LICENSE (Version 2, June 1991). 
+  * Please see the LICENSE included with this distribution for details.
+  * Author: Stefaan Ternier
+ **/
+ 
+ (function() {
 	ar.google.fusion = {};
 	ar.google.fu = ar.google.fusion;
 	var googleUrl = 'https://www.google.com';
--- Resources/arlearn/google/login.js
+++ Resources/arlearn/google/login.js
@@ -1,3 +1,10 @@
+/**
+ * ARLearn Platform
+ * Copyright (c) 2009-2011 by Open Universiteit. All Rights Reserved.
+ * ARLearn2 is licensed under the terms of GNU GENERAL PUBLIC LICENSE (Version 2, June 1991). 
+  * Please see the LICENSE included with this distribution for details.
+  * Author: Stefaan Ternier
+ **/
 
 (function() {
 
